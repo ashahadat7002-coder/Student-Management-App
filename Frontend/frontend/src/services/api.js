b7f5e2fca80cdbe5c926223bb898cff4b4912306
ashahadat7002-coder <ashahadat7002@gmail.com>
2026-07-10 21:36:44 +0530

    FIXED API URL

--- Frontend/frontend/src/services/api.js
+++ Frontend/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/students",
+    baseURL: "https://student-management-app-1-u7yr.onrender.com",
 });
 export default API;
